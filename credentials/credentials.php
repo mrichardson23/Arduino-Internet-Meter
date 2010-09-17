807818454d3964020921e3e029d5a936ab6b03d2
Matt Richardson <mrichardson23@gmail.com>
2010-09-17 11:50:57 -0700

    Clarify readerID variable.

--- credentials/credentials.php
+++ credentials/credentials.php
@@ -1,5 +1,11 @@
 <?php
-//Parameters required to login to Google
+// Parameters required to login to Google.
+
+// To get Reader ID, pull the following URL while logged into your Google Account:
+// http://www.google.com/reader/api/0/unread-count 
+// The reader ID is found in the response:
+// user/READER_ID_HERE/state/com.google/reading-list
+
 $user="GOOGLEUSERNAME@gmail.com";
 $pass="GOOGLE PASSWORD";
 $readerId="readerID here";
